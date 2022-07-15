3970450e5a920ccd025c7e75a2b4fa78549ac750
AlphaSgh <upupsgh@163.com>
2022-07-15 10:13:38 +0800

    2022-07-15

--- 19.go_http/go_http.go
+++ 19.go_http/go_http.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"os"
 	"strings"
 )
 
@@ -47,8 +49,40 @@ func http1() {
 	}
 	fmt.Printf("%s", body)
 }
+
+func http2() {
+	debug := os.Getenv("DEBUG")
+	client := &http.Client{}
+	request, err := http.NewRequest("GET", "https://ifconfig.io/", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if debug == "1" {
+		debugRequest, err := httputil.DumpRequestOut(request, true)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s", debugRequest)
+	}
+	response, err := client.Do(request)
+	defer response.Body.Close()
+	if debug == "1" {
+		debugResponse, err := httputil.DumpResponse(response, true)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s", debugResponse)
+	}
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", body)
+}
+
 func main() {
-	http1()
+	http2()
+	// http1()
 	// httpPut()
 	// MyHttp()
 }
